fe07344dfb4d4480352b67c3fbcb5ab4ce68cfce
Matthew Franchini <matchewf11@gmail.com>
2025-06-03 00:40:25 -0700

    Fixed up day one stuff to be neater

--- day1/partone.lua
+++ day1/partone.lua
@@ -1,42 +1,19 @@
-io.input("input.txt")
+io.input("test.txt")
 
-local count, first, second = 0, {}, {}
-
-local currNum = io.read("*number")
-while currNum ~= nil do
-	if count % 2 == 0 then
-		first[#first + 1] = currNum
-	else
-		second[#second + 1] = currNum
-	end
-	count = count + 1
-	currNum = io.read("*number")
+local line = io.read()
+local left, right = {}, {}
+while line do
+	local _, _, x, y = string.find(line, "(%d+)   (%d+)")
+	left[#left + 1] = x
+	right[#right + 1] = y
+	line = io.read()
 end
 
-function BubbleSort(arr)
-	for i = 1, #arr - 1 do
-		for j = 1, #arr - i do
-			if arr[j] > arr[j + 1] then
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-			end
-		end
-	end
-end
-
-BubbleSort(first)
-BubbleSort(second)
+table.sort(left)
+table.sort(right)
 
 local sum = 0
-
-for j, firstVal in ipairs(first) do
-	local secVal = second[j]
-	local dif = firstVal - secVal
-	if dif < 0 then
-		dif = dif * -1
-	end
-	sum = sum + dif
+for i = 1, #left do
+	sum = sum + math.abs(left[i] - right[i])
 end
-
 print(sum)
-
-io.close()
